64f6843951f67a99e6eb343e90b071a70c53ff3f
samulot <lotti.samuel@gmail.com>
2020-08-06 14:44:50 -0300

    criando rota home

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://https://itau-turma6a-samuel.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
